9e2df061a9daa351cbc85b9fc1bf6f9fb2d75332
diabolicfreak <diabolicfreak@gmail.com>
2014-12-18 21:12:34 +0000

    5 Designing home page

---
+++ app/views/pages/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.default')
+
+@section('content')
+    <div class="jumbotron">
+            <h1>Welcome to Larabook</h1>
+           <p class="lead">Go ahead Sign Up and see what the fuss is all about.</p>
+            <p><a class="btn btn-lg btn-success" href="#" role="button">Sign up</a></p>
+    </div>
+@stop
